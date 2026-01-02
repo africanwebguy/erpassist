eae0584ccb4fbafda4bf445225002d4f5e86e5e5
Isaactakyi <129322401+africanwebguy@users.noreply.github.com>
2026-01-02 23:53:47 +0000

    trial

--- erpassist/hooks.py
+++ erpassist/hooks.py
@@ -14,25 +14,6 @@ app_license = "MIT"
 app_include_css = "/assets/erpassist/css/erpassist.css"
 app_include_js = "/assets/erpassist/js/erpassist.bundle.js"
 
-# include js, css files in header of web template
-# web_include_css = "/assets/erpassist/css/erpassist.css"
-# web_include_js = "/assets/erpassist/js/erpassist.js"
-
-# include custom scss in every website theme (without file extension ".scss")
-# website_theme_scss = "erpassist/public/scss/website"
-
-# include js, css files in header of web form
-# webform_include_js = {"doctype": "public/js/doctype.js"}
-# webform_include_css = {"doctype": "public/css/doctype.css"}
-
-# include js in page
-# page_js = {"page" : "public/js/file.js"}
-
-# include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
-# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
-# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
 # Home Pages
 # ----------
